62a1cea0f2a04b1d5c34ddee8cdc32cc1041e1f0
MayaGueguen <maya.gueguen@gmail.com>
2018-04-17 14:01:18 +0200

    Add tests to speciesClustering_step1.

--- tests/testthat/test-PRE_FATE.speciesClustering_step1.R
+++ tests/testthat/test-PRE_FATE.speciesClustering_step1.R
@@ -38,66 +38,24 @@ test_that("PRE_FATE.speciesClustering_step1 gives error with wrong data", {
                , "`mat.species.DIST` contain NA values ", fixed = T)
 })
 
-# ## OUTPUTS
-# test_that("PRE_FATE.speciesClustering_step1 of BB values give right results", {
-#   expect_equal(PRE_FATE.speciesClustering_step1("r"), 0.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1("+"), 0.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1("1"), 3)
-#   expect_equal(PRE_FATE.speciesClustering_step1("2"), 15)
-#   expect_equal(PRE_FATE.speciesClustering_step1("3"), 37.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1("4"), 62.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1("5"), 87.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor("r")), 0.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor("+")), 0.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor("1")), 3)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor("2")), 15)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor("3")), 37.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor("4")), 62.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor("5")), 87.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(1), 3)
-#   expect_equal(PRE_FATE.speciesClustering_step1(2), 15)
-#   expect_equal(PRE_FATE.speciesClustering_step1(3), 37.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(4), 62.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(5), 87.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor(1)), 3)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor(2)), 15)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor(3)), 37.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor(4)), 62.5)
-#   expect_equal(PRE_FATE.speciesClustering_step1(factor(5)), 87.5)
-# })
-# 
-# ## OUTPUTS
-# test_that("PRE_FATE.speciesClustering_step1 of none BB values give NA", {
-#   expect_equal(PRE_FATE.speciesClustering_step1(-1), as.numeric(NA))
-#   expect_equal(PRE_FATE.speciesClustering_step1(6), as.numeric(NA))
-#   expect_equal(PRE_FATE.speciesClustering_step1(19856394), as.numeric(NA))
-#   expect_equal(PRE_FATE.speciesClustering_step1("a"), as.numeric(NA))
-#   expect_equal(PRE_FATE.speciesClustering_step1("abc"), as.numeric(NA))
-# })
-# 
-# ## OUTPUTS
-# test_that("PRE_FATE.speciesClustering_step1 of missing give NA", {
-#   expect_equal(PRE_FATE.speciesClustering_step1(NA), as.numeric(NA))
-#   expect_equal(PRE_FATE.speciesClustering_step1("NA"), as.numeric(NA))
-# })
-# 
-# ## OUTPUTS
-# test_that("PRE_FATE.speciesClustering_step1 does not modify length", {
-#   expect_equal(length(PRE_FATE.speciesClustering_step1(NA)), 1)
-#   expect_equal(length(PRE_FATE.speciesClustering_step1("NA")), 1)
-#   expect_equal(length(PRE_FATE.speciesClustering_step1("1")), 1)
-#   expect_equal(length(PRE_FATE.speciesClustering_step1(c("1", NA))), 2)
-#   expect_equal(length(PRE_FATE.speciesClustering_step1(c("1", "NA"))), 2)
-#   expect_equal(length(PRE_FATE.speciesClustering_step1(c(NA, NA))), 2)
-#   expect_equal(length(PRE_FATE.speciesClustering_step1(c(12, -789))), 2)
-# })
-# 
-# ## OUTPUTS
-# test_that("PRE_FATE.speciesClustering_step1 gives numeric output", {
-#   expect_output(str(PRE_FATE.speciesClustering_step1(NA)), "num")
-#   expect_output(str(PRE_FATE.speciesClustering_step1(1)), "num")
-#   expect_output(str(PRE_FATE.speciesClustering_step1("1")), "num")
-#   expect_output(str(PRE_FATE.speciesClustering_step1(c(1, NA))), "num")
-# })
-
-
+## OUTPUTS
+test_that("PRE_FATE.speciesClustering_step1 gives right output", {
+  expect_output(str(PRE_FATE.speciesClustering_step1(matrix(seq(9), ncol=3))), "List")
+  expect_equal(length(PRE_FATE.speciesClustering_step1(matrix(seq(9), ncol=3))), 2)
+  
+  expect_output(str(PRE_FATE.speciesClustering_step1(matrix(seq(9), ncol=3))$clust.dendograms), "List")
+  expect_equal(length(PRE_FATE.speciesClustering_step1(matrix(seq(9), ncol=3))$clust.dendograms), 1)
+  
+  expect_output(str(PRE_FATE.speciesClustering_step1(list(matrix(seq(9), ncol=3)
+                                                          , matrix(seq(9), ncol=3)))$clust.dendograms), "List")
+  expect_equal(length(PRE_FATE.speciesClustering_step1(list(matrix(seq(9), ncol=3)
+                                                            , matrix(seq(9), ncol=3)))$clust.dendograms), 2)
+  
+  expect_output(str(PRE_FATE.speciesClustering_step1(matrix(seq(9), ncol=3))$clust.evaluation), "data.frame")
+  expect_output(str(PRE_FATE.speciesClustering_step1(matrix(seq(9), ncol=3))$clust.evaluation), "4 variables")
+  
+  expect_output(str(PRE_FATE.speciesClustering_step1(list(matrix(seq(9), ncol=3)
+                                                          , matrix(seq(9), ncol=3)))$clust.evaluation), "data.frame")
+  expect_output(str(PRE_FATE.speciesClustering_step1(list(matrix(seq(9), ncol=3)
+                                                          , matrix(seq(9), ncol=3)))$clust.evaluation), "4 variables")
+})
