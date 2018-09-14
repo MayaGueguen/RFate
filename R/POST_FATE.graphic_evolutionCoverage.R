8ca64c7c7e921e3639b0f18219c3f67938f39f26
MayaGueguen <maya.gueguen@gmail.com>
2018-09-14 09:54:12 +0200

    Add description and details for POST_FATE.graphic_evolutionCoverage. Remove warning messages from .unzip and .zip functions. Retest and compute html web pages.

--- R/POST_FATE.graphic_evolutionCoverage.R
+++ R/POST_FATE.graphic_evolutionCoverage.R
@@ -1,106 +1,79 @@
 ### HEADER #####################################################################
 ##' @title Create a graphical representation of the evolution of PFG coverage
-##' for a \code{FATE-HD} simulation
+##' and abundance through time for a \code{FATE-HD} simulation
 ##' 
 ##' @name POST_FATE.graphic_evolutionCoverage
 ##'
 ##' @author Maya Guéguen
 ##' 
-##' @description This script is designed to create a parameter file containing
-##' simulation years at which the \code{FATE-HD} software must save rasters of
-##' PFG abundances or simulation objects.
+##' @description This script is designed to produce two graphical 
+##' representations for a \code{FATE-HD} simulation : 1) the evolution through
+##' time of the space occupation of each PFG ; 2) the evolution through time
+##' of the abundance of each PFG.
 ##'              
 ##' @param name.simulation a \code{string} that corresponds to the main directory
 ##' or simulation name of the \code{FATE-HD} simulation
-##' @param file.simulParam \emph{to be filled}
+##' @param file.simulParam a \code{string} that corresponds to the name of a
+##' parameter file that will be contained into the \code{PARAM_SIMUL} folder
+##' of the \code{FATE-HD} simulation
+##' @param no.years an \code{integer} corresponding to the number of simulation
+##' years that will be used to extract PFG abundance maps
+##' @param abund.fixedScale default \code{TRUE}. If \code{FALSE}, the ordinate
+##' scale will be adapted for each PFG for the graphical representation of the 
+##' evoluation of abundances through time
 ##' @param opt.no_CPU default 1 (\emph{optional}). The number of resources that 
 ##' can be used to parallelize the \code{unzip/zip} of raster files
 ##' 
+##' 
 ##' @details 
 ##' 
-##' \emph{to be filled}
+##' This function allows one to obtain, for a specific \code{FATE-HD} simulation
+##' and a specific parameter file within this simulation, two preanalytical
+##' graphics. \cr
+##' 
+##' For each PFG and each selected simulation year, raster maps are retrieved
+##' from the results folder \code{ABUND_perPFG_allStrata} and unzipped.
+##' Informations extracted lead to the production of two graphics before the
+##' maps are compressed again :
+##' 
+##' \itemize{
+##'   \item{The evolution of \strong{space occupancy} of each Plant Functional
+##'   Group through simulation time.}
+##'   \item{The evolution of \strong{abundance} of each Plant Functional
+##'   Group through simulation time.} 
+##' }
 ##' 
 ##' 
-##' @return T
 ##' 
+##' @return Two \code{POST_FATE_[...].pdf} files are created : 
+##' \describe{
+##'   \item{\file{GRAPHIC_A \cr spaceOccupancy}}{to visualize for each PFG the
+##'   evolution of its occupation of the studied area through simulation time}
+##'   \item{\file{GRAPHIC_B \cr abundance}}{to viualize for each PFG the
+##'   evolution of its abundance within the whole studied area through
+##'   simulation time}
+##' }
 ##' 
+##'  
 ##' @examples
 ##' 
-##' ## Create a skeleton folder with the default name ('FATE_simulation')
-##' if (dir.exists("FATE_simulation")) system("rm -r FATE_simulation/")
-##' PRE_FATE.skeletonDirectory()
-##' 
-##' ## Create a Namespace_constants parameter file
-##' PRE_FATE.params_namespaceConstants(name.simulation = "FATE_simulation"
-##'                                    , global.abund.low = 1000000
-##'                                    , global.abund.med = 5000000
-##'                                    , global.abund.high = 8000000
-##'                                    , global.max.by.cohort = 5000000
-##'                                    , global.resource.thresh.med = 13000000
-##'                                    , global.resource.thresh.low = 19000000)
-##' 
-##' ## Create a Global_parameters file
-##' PRE_FATE.params_globalParameters(name.simulation = "FATE_simulation"
-##'                                  , required.succ_option = "fateh"
-##'                                  , required.no_PFG = 6
-##'                                  , required.no_STRATA = 5
-##'                                  , required.hs_option = 1
-##'                                  , required.seeding_timestep = 1
-##'                                  , required.seeding_duration = c(10,50)
-##'                                  , required.simul_duration = 100)
-##' 
-##' ## Create a SAVE_year_maps or/and SAVE_year_objects parameter file
-##' PRE_FATE.params_saveYears(name.simulation = "FATE_simulation"
-##'                           , years.maps = c(100, 150, 200)
-##'                           , years.objects = 200)
-##' 
-##' ## Create a Changing_times parameter file
-##' PRE_FATE.params_changingYears(name.simulation = "FATE_simulation"
-##'                               , type.changing = "DIST"
-##'                               , mat.changing = data.frame(year = c(50,50,80,80)
-##'                                                           , order = c(1,2,1,2)
-##'                                                           , file.name = c("MASK_DIST1_50.tif"
-##'                                                                           , "MASK_DIST2_50.tif"
-##'                                                                           , "MASK_DIST1_80.tif"
-##'                                                                           , "MASK_DIST2_80.tif")))
-##' 
-##' ## Create PFG succession parameter files
-##' PRE_FATE.params_PFGsuccession(name.simulation = "FATE_simulation"
-##'                               , mat.PFG.succ = data.frame(PFG = paste0("PFG",1:6)
-##'                                                           , type = c("C", "C", "H", "H", "P", "P")
-##'                                                           , height = c(10, 250, 36, 68, 1250, 550)
-##'                                                           , maturity = c(5, 5, 3, 3, 8, 9)
-##'                                                           , longevity = c(12, 200, 25, 4, 110, 70)
-##'                                                           , dispersal = 1
-##'                                                           , light = c(4, 6, 3, 6, 5, 5)))
-##' 
-##' ## Create PFG dispersal parameter files
-##' PRE_FATE.params_PFGdispersal(name.simulation = "FATE_simulation"
-##'                              , mat.PFG.disp = data.frame(PFG = paste0("PFG", 1:6)
-##'                                                          , d50 = rep(c(500, 500, 100),2)
-##'                                                          , d99 = rep(c(10000, 15000, 20000),2)
-##'                                                          , ldd = rep(c(100000, 50000, 100000),2)))
-##' 
-##' ## Create PFG disturbance parameter files
-##' PRE_FATE.params_PFGdisturbance(name.simulation = "FATE_simulation"
-##'                                , mat.PFG.dist = data.frame(name = rep(c("DIST1","DIST2"), each = 4)
-##'                                                            , responseStage = rep(1:4, 2)
-##'                                                            , KilledIndiv_H = c(0,0,0,0,1,1,0,0)
-##'                                                            , KilledIndiv_C = c(0,10,10,10,1,1,0,0)
-##'                                                            , KilledIndiv_P = c(10,10,10,10,10,0,0,0)
-##'                                                            , ResproutIndiv_H = c(0,0,9,10,0,0,5,1)
-##'                                                            , ResproutIndiv_C = c(0,0,0,0,0,0,5,1)
-##'                                                            , ResproutIndiv_P = c(0,0,0,0,0,0,0,0)))
-##' 
-##' ## Create a Simulation parameter file
-##' file.create("FATE_simulation/DATA/MASK/mask.tif")
-##' PRE_FATE.params_simulParameters(name.simulation = "FATE_simulation"
-##'                                 , name.mask = "mask.tif")
-##'                                 
-##'                                 
-##' POST_FATE.graphic_evolutionCoverage(name.simulation
+##' \dontrun{                      
+##' POST_FATE.graphic_evolutionCoverage(name.simulation = "FATE_simulation"
 ##'                                     , file.simulParam = "Simul_parameters_V1.txt"
 ##'                                     , opt.no_CPU = 1)
+##'                                     
+##' POST_FATE.graphic_evolutionCoverage(name.simulation = "FATE_simulation"
+##'                                     , file.simulParam = "Simul_parameters_V1.txt"
+##'                                     , no.years = 50
+##'                                     , opt.no_CPU = 4)
+##'                                     
+##' POST_FATE.graphic_evolutionCoverage(name.simulation = "FATE_simulation"
+##'                                     , file.simulParam = "Simul_parameters_V1.txt"
+##'                                     , no.years = 50
+##'                                     , abund.fixedScale = FALSE
+##'                                     , opt.no_CPU = 1)
+##' }
+##'                                     
 ##'                                     
 ##' 
 ##' @export
@@ -117,15 +90,13 @@
 ##'
 ## END OF HEADER ###############################################################
 
-setwd("~/FATE_Bauges/")
-POST_FATE.graphic_evolutionCoverage(name.simulation = "FATE_Bauges"
-                                    , file.simulParam = "paramSimul_Graz1_CA_rcp26.txt"
-                                    , opt.no_CPU = 7)
 
 
 POST_FATE.graphic_evolutionCoverage = function(
   name.simulation
   , file.simulParam = NULL
+  , no.years = 10
+  , abund.fixedScale = TRUE
   , opt.no_CPU = 1
 ){
   
@@ -171,7 +142,7 @@ POST_FATE.graphic_evolutionCoverage = function(
     years = sapply(sub("Abund_YEAR_", "", raster.perPFG.allStrata)
                    , function(x) strsplit(as.character(x), "_")[[1]][1])
     years = sort(unique(as.numeric(years)))
-    years = years[round(seq(1, length(years), length.out = 10))]
+    years = years[round(seq(1, length(years), length.out = min(no.years, length(years))))]
     no_years = length(years)
     
     ## Get number of PFGs ----------------------------------------------------------
@@ -226,10 +197,13 @@ POST_FATE.graphic_evolutionCoverage = function(
       file_name = file_name[which(file.exists(file_name))]
       
       ras = stack(file_name) * ras.mask
+      ras = as.data.frame(ras)
       ## calculate the % of cover of each PPFG
-      distri[as.character(y), gp] = length(which(ras[ind_1_mask] > 0)) / no_1_mask
-      distriAbund[as.character(y), gp] = sum(ras[ind_1_mask], na.rm = T)
+      distri[as.character(y), gp] = apply(ras, 2, function(x) length(which(x[ind_1_mask] > 0)) / no_1_mask)
+      distriAbund[as.character(y), gp] = apply(ras, 2, function(x) sum(x[ind_1_mask], na.rm = T))
     }
+    cat("\n")
+    
     distri.melt = melt(distri)
     colnames(distri.melt) = c("YEAR", "PFG", "Abund")
     distriAbund.melt = melt(distriAbund)
@@ -238,7 +212,7 @@ POST_FATE.graphic_evolutionCoverage = function(
     ## produce the plot ------------------------------------------------------------
     
     ## Evolution of space occupation
-    pp1 = ggplot(distri.melt, aes_string(x = "YEAR", y = "Abund", group = "PFG")) +
+    pp1 = ggplot(distri.melt, aes_string(x = "YEAR", y = "Abund * 100", group = "PFG")) +
       geom_line() +
       facet_wrap("~ PFG") +
       labs(x = "", y = "", title = paste0("GRAPH A : evolution of species' space occupation"),
@@ -251,7 +225,7 @@ POST_FATE.graphic_evolutionCoverage = function(
     ## Evolution of abundance
     pp2 = ggplot(distriAbund.melt, aes_string(x = "YEAR", y = "Abund", group = "PFG")) +
       geom_line() +
-      facet_wrap("~ PFG") +
+      facet_wrap("~ PFG", scales = ifelse(abund.fixedScale, "fixed", "free_y")) +
       labs(x = "", y = "", title = paste0("GRAPH B : evolution of species' abundance"),
            subtitle = paste0("For each PFG, the line represents the evolution through time of its abundance\n",
                              "over the whole studied area, meaning the sum of its abundances in every pixel.\n")) +
