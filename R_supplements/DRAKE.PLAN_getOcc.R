5f56d249311dadb039d7ac4b47958f0a6ec2a175
MayaGueguen <maya.gueguen@gmail.com>
2019-01-09 09:18:36 +0100

    R_supplements : - add DRAKE plan to get dominant species - add functions to get database occurrences and dominant species

---
+++ R_supplements/DRAKE.PLAN_getOcc.R
@@ -0,0 +1,87 @@
+
+rm(list = ls())
+
+library(RPostgreSQL)
+library(foreign)
+library(data.table)
+library(RFate)
+library(drake)
+
+source("RFate/R_supplements/DRAKE.PRE_FATE.data_getDB_occ.R")
+source("RFate/R_supplements/DRAKE.PRE_FATE.data_getOccDominantSpecies.R")
+
+path.data = "RFate/data/"
+setwd(path.data)
+
+################################################################################################################
+### 1. GET DB VALUES - GATHER PER SPECIES
+################################################################################################################
+
+# clean()
+PLAN.getOCC = drake_plan(
+  zone.name = "Bauges"
+  , zone.extent = c(928136, 966148, 6491251, 6537475)
+  , create_wd = dir.create(path = zone.name)
+  ## Get DB
+  , OCC.DB = getDB(x.min = zone.extent[1]
+                   , x.max = zone.extent[2]
+                   , y.min = zone.extent[3]
+                   , y.max = zone.extent[4])
+  ## Get species
+  , species = OCC.DB$species[, c("numtaxon", "genre", "libcbna")]
+  ## Get sites informations
+  , stations = OCC.DB$stations[, c("numchrono", "coderqualif", "longitudel93_rel", "latitudel93_rel")]
+  , stations.COMMUNITY = stations$numchrono[which(stations$coderqualif %in% c("R06", "R07"))]
+  , XY = getOcc_1_XY(stations = stations
+                     , zone.name = zone.name)
+  ## Get occurrences
+  , observations = OCC.DB$observations[, c("numchrono", "numtaxon", "codecover")]
+  , observations.xy = getOcc_1_obs(observations = observations
+                                   , stations = stations)
+  , sp.dom = getOcc_2_selectDom(observations.xy = observations.xy
+                                , species = species
+                                , zone.name = zone.name)
+  , sp.dom.mat = getOcc_3_matDom(sp.SELECT = sp.dom
+                                 , observations.xy = observations.xy
+                                 , stations.COMMUNITY = stations.COMMUNITY
+                                 , zone.name = zone.name)
+  , sp.dom.occ = getOcc_3_occDom(mat.sites.species = sp.dom.mat
+                                 , species = species
+                                 , zone.name = zone.name)
+  , strings_in_dots = "literals"
+)
+
+vis_drake_graph(drake_config(PLAN.getOCC)
+                , targets_only = TRUE)
+make(PLAN.getOCC)
+vis_drake_graph(drake_config(PLAN.getOCC)
+                , targets_only = TRUE)
+
+# loadd(traits.quant.med)
+# loadd(traits.quali.med)
+
+################################################################################################################
+### 2. KEEP ONLY TRAITS OF INTEREST FOR FATE
+################################################################################################################
+
+# clean()
+# PLAN.FATE = drake_plan(traits.quant = read.csv(file_in("TRAITS_quantitative_median_190107.csv"), stringsAsFactors = F, sep = "\t")
+#                        , traits.quali = read.csv(file_in("TRAITS_qualitative_190107.csv"), stringsAsFactors = F, sep = "\t")
+#                        , traits_names = get_traits_names()
+#                        , TAB_traits = keep_traits_FATE(traits.quant, traits.quali, traits_names)
+#                        , TAB_traits_FATE = reorganize_traits_FATE(TAB_traits)
+#                        , TAB_traits_FATE.written = fwrite(TAB_traits_FATE, file = file_out("TRAITS_FATE_190107.csv"), sep = "\t")
+#                        , strings_in_dots = "literals"
+# )
+# 
+# vis_drake_graph(drake_config(PLAN.FATE)
+#                 , targets_only = TRUE)
+# make(PLAN.FATE)
+# vis_drake_graph(drake_config(PLAN.FATE)
+#                 , targets_only = TRUE)
+# 
+# loadd(TAB_traits)
+# summary(TAB_traits)
+# 
+# loadd(TAB_traits_FATE)
+# summary(TAB_traits_FATE)
---
+++ R_supplements/DRAKE.PRE_FATE.data_getOccDominantSpecies.R
@@ -0,0 +1,110 @@
+
+
+################################################################################################################
+### 1. GET OCCURRENCES
+################################################################################################################
+
+getOcc_1_XY = function(stations, zone.name)
+{
+  stations.XY = stations[, c("numchrono", "longitudel93_rel", "latitudel93_rel")]
+  stations.XY = unique(stations.XY)
+  XY = data.frame(stations.XY[, c("longitudel93_rel", "latitudel93_rel")]
+                  , row.names = paste0("NUMCHRONO-", stations.XY$numchrono))
+  colnames(XY) = c("X_L93", "Y_L93")
+  
+  save(XY, file = paste0(zone.name, "/XY.RData"))
+  
+  return(XY)
+}
+
+getOcc_1_obs = function(observations, stations)
+{
+  observations$codecover = PRE_FATE.abundBraunBlanquet(observations$codecover)
+  observations.xy = merge(observations, stations, by = "numchrono")
+  head(observations.xy)
+  
+  return(observations.xy)
+}
+
+
+################################################################################################################
+### 2. SELECT DOMINANT SPECIES
+################################################################################################################
+
+getOcc_2_selectDom = function(observations.xy, species, zone.name)
+{
+  setwd(zone.name)
+  
+  occ = observations.xy[, c("numchrono", "numtaxon", "codecover", "longitudel93_rel", "latitudel93_rel")]
+  occ = unique(occ) ## remove strata redundancy
+  colnames(occ) = c("sites", "species", "abund", "X", "Y")
+  
+  ## SELECT DOMINANT SPECIES
+  sp.SELECT = PRE_FATE.selectDominant(mat.site.species.abund = occ
+                                      , selectionRule.quanti = 0.8
+                                      , selectionRule.min_mean_abund = 10
+                                      , selectionRule.min_no_abund_over25 = 20)
+  
+  ## Get species names
+  sp.SELECT = merge(species, sp.SELECT, by.x = "numtaxon", by.y = "species", all.y = TRUE)
+  
+  setwd("./../")
+  
+  return(sp.SELECT)
+}
+
+
+################################################################################################################
+### 3. GET DOMINANT SPECIES OBSERVATIONS
+################################################################################################################
+
+getOcc_3_matDom = function(sp.SELECT, observations.xy, stations.COMMUNITY, zone.name)
+{
+  ## Get dominant species observations
+  sp.SELECT.occ = merge(sp.SELECT[, c("numtaxon", "genre", "libcbna")], observations.xy, by = "numtaxon")
+  
+  ## Transform into sites x species matrix
+  mat.sites.species = tapply(X = sp.SELECT.occ$codecover
+                             , INDEX = list(sp.SELECT.occ$numchrono, sp.SELECT.occ$numtaxon)
+                             , FUN = length)
+  ind.COMMUNITY = which(rownames(mat.sites.species) %in% stations.COMMUNITY)
+  for(i in ind.COMMUNITY)
+  {
+    mat.sites.species[i, ] = ifelse(is.na(mat.sites.species[i, ]), 0, 1)
+  }
+  dim(mat.sites.species)
+  mat.sites.species[1:10, 1:10]
+  
+  save(mat.sites.species, file = paste0(zone.name, "/mat.sites.species.RData"))
+  
+  return(mat.sites.species)
+}
+
+getOcc_3_occDom = function(mat.sites.species, species, zone.name)
+{
+  ## Create species occurrences files
+  dir.create(paste0(zone.name, "/SP_OCC"))
+  
+  sp.suppr = vector()
+  for(sp in colnames(mat.sites.species))
+  {
+    sp.occ = mat.sites.species[, sp]
+    sp.occ = sp.occ[which(!is.na(sp.occ))]
+    
+    if (length(which(sp.occ == 0)) == 0 || length(which(sp.occ == 1)) < 20) 
+    {
+      sp.suppr = c(sp.suppr, sp)
+    } else
+    {
+      save(sp.occ, file = paste0(zone.name, "/SP_OCC/OCC_X", sp))
+    }
+  }
+  cat(" ==> No absence data for :", sp.suppr)
+  dom_missing = species[which(species$numtaxon %in% sp.suppr), ]
+  write.csv(dom_missing, file = paste0(zone.name, '/MISSING_dominant_species_observations.csv'), row.names = F)
+}
+
+################################################################################################################
+### 4. BUILD SDM FOR DOMINANT SPECIES
+################################################################################################################
+
