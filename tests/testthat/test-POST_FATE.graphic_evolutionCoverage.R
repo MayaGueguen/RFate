d86739905928d1da22941ee4d1ba9dfab3bc42d7
MayaGueguen <maya.gueguen@gmail.com>
2019-02-27 09:43:03 +0100

    Try to improve test coverage for POST_FATE.graphic_evolutionCoverage.

--- tests/testthat/test-POST_FATE.graphic_evolutionCoverage.R
+++ tests/testthat/test-POST_FATE.graphic_evolutionCoverage.R
@@ -188,26 +188,11 @@ test_that("POST_FATE.graphic_evolutionCoverage gives error with wrong data : ras
 
   cat("ncols 3\nnrows 3\nxllcorner 1\nyllcorner 1\ncellsize 30\nnodata_value -999\n0 0 1\n0 1 1\n1 1 1"
       , file = "FATE_simulation/RESULTS/Hello/ABUND_perPFG_allStrata/Abund_YEAR_1_Hop_STRATA_all.tif")
-  # expect_error(POST_FATE.graphic_evolutionCoverage(name.simulation = "FATE_simulation"
-  #                                                  , file.simulParam = "ParamSimul.txt")
-  #              # , "objet 'distri' introuvable"
-  #              , "Cannot create RasterLayer object from this file"
-  #              , fixed = TRUE)
-  # 
-  # expect_error(POST_FATE.graphic_evolutionCoverage(name.simulation = "FATE_simulation")
-  #              # , "objet 'distri' introuvable"
-  #              , "Cannot create RasterLayer object from this file"
-  #              , fixed = TRUE)
-  
-  
-  # expect_output(str(POST_FATE.graphic_validationStatistics(name.simulation = "FATE_simulation"
-  #                                                          , file.simulParam = "ParamSimul.txt"
-  #                                                          , year = 10
-  #                                                          , mat.PFG.obs = data.frame(PFG = "A", X = 2, Y = 3, obs = 0, stringsAsFactors = F)))
-  #               , "List")
-  # expect_output(str(POST_FATE.graphic_validationStatistics(name.simulation = "FATE_simulation"
-  #                                                          , year = 10
-  #                                                          , mat.PFG.obs = data.frame(PFG = "A", X = 2, Y = 3, obs = 0, stringsAsFactors = F)))
-  #               , "List")
+
+  expect_message(POST_FATE.graphic_evolutionCoverage(name.simulation = "FATE_simulation"
+                                                     , file.simulParam = "ParamSimul.txt")
+                 , "have been successfully created !")
+  expect_message(POST_FATE.graphic_evolutionCoverage(name.simulation = "FATE_simulation")
+                 , "have been successfully created !")
 })
 
