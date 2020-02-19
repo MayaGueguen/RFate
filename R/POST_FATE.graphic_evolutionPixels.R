9dca5af30c1a834606ff23a8a2f68b5b458fdb13
MayaGueguen <maya.gueguen@gmail.com>
2020-02-19 17:20:46 +0100

    Add soil and light, and adapt graphic, in POST_FATE.graphic_evolutionPixels function.

--- R/POST_FATE.graphic_evolutionPixels.R
+++ R/POST_FATE.graphic_evolutionPixels.R
@@ -79,9 +79,12 @@
 ##' @importFrom foreach foreach
 ##' 
 ##' @importFrom ggplot2 ggplot aes aes_string ggsave
-##' geom_line geom_point geom_hline geom_vline geom_label geom_errorbar geom_path
+##' geom_line geom_point geom_hline geom_vline geom_label 
+##' geom_errorbar geom_path geom_area
 ##' element_text element_blank element_rect
-##' scale_color_discrete scale_color_manual scale_shape_manual facet_grid labs theme
+##' scale_color_discrete scale_color_manual 
+##' scale_shape_manual scale_fill_manual
+##' facet_grid labs theme
 ##' @importFrom ggthemes theme_fivethirtyeight
 ##' @importFrom ggrepel geom_label_repel
 ##'
@@ -146,15 +149,51 @@ POST_FATE.graphic_evolutionPixels = function(
     .testParam_existFile(file.abundance)
     tab.abundance = fread(file.abundance)
     tab.abundance = as.data.frame(tab.abundance)
+    tab.abundance$TYPE = "abundance"
+    colnames(tab.abundance)[which(colnames(tab.abundance) == "PFG")] = "GROUP"
     
     years = colnames(tab.abundance)
-    years = years[which(!(years %in% c("PFG", "ID", "X", "Y", "HAB")))]
+    years = years[which(!(years %in% c("TYPE", "GROUP", "ID", "X", "Y", "HAB")))]
     years = as.numeric(years)
     no_years = length(years)
     
+    strata = paste0("Stratum ", (no_STRATA - 1):0)
+    
+    ## Get resources tables --------------------------------------------------------
+    if (doLight)
+    {
+      file.light = paste0(name.simulation
+                              , "/RESULTS/POST_FATE_evolution_light_PIXEL_"
+                              , basename(dir.save)
+                              , ".csv")
+      .testParam_existFile(file.light)
+      tab.light = fread(file.light)
+      tab.light = as.data.frame(tab.light)
+      tab.light$STRATUM = paste0("Stratum ", tab.light$STRATUM)
+      tab.light$TYPE = "light"
+      colnames(tab.light)[which(colnames(tab.light) == "STRATUM")] = "GROUP"
+      
+      tab.abundance = rbind(tab.abundance, tab.light)
+    }
+    
+    if (doSoil)
+    {
+      file.soil = paste0(name.simulation
+                          , "/RESULTS/POST_FATE_evolution_soil_PIXEL_"
+                          , basename(dir.save)
+                          , ".csv")
+      .testParam_existFile(file.soil)
+      tab.soil = fread(file.soil)
+      tab.soil = as.data.frame(tab.soil)
+      tab.soil$GROUP = "soil"
+      tab.soil$TYPE = "soil"
+      
+      tab.abundance = rbind(tab.abundance, tab.soil)
+    }
+
+    
     ## Get concerned cells id ------------------------------------------------------
-    # IDS = sample(ind_1_mask, 5)
-    IDS = sample(tab.abundance$ID, 5)
+    IDS = sample(unique(tab.abundance$ID), 5)
     if (!is.null(opt.cells_ID))
     {
       if (sum(opt.cells_ID %in% ind_1_mask) == length(opt.cells_ID))
@@ -177,14 +216,16 @@ POST_FATE.graphic_evolutionPixels = function(
     
     ## Transform the data inside the table -----------------------------------------
     distriAbund = tab.abundance[which(tab.abundance$ID %in% IDS), , drop = FALSE]
-    distriAbund = distriAbund[, c("PFG", "ID", "HAB", as.character(years))]
-    distriAbund = melt(distriAbund, id.vars = c("PFG", "ID", "HAB"))
-    colnames(distriAbund) = c("PFG", "ID", "HAB", "YEAR", "Abund")
+    distriAbund = distriAbund[, c("TYPE", "GROUP", "ID", "HAB", as.character(years))]
+    distriAbund = melt(distriAbund, id.vars = c("TYPE", "GROUP", "ID", "HAB"))
+    colnames(distriAbund) = c("TYPE", "GROUP", "ID", "HAB", "YEAR", "value")
     distriAbund$YEAR = as.numeric(as.character(distriAbund$YEAR))
+    distriAbund$TYPE = factor(distriAbund$TYPE, c("light", "abundance", "soil"))
+    distriAbund$GROUP = factor(distriAbund$GROUP, c(strata, PFG, "soil"))
     
     write.csv(distriAbund
               , file = paste0(name.simulation
-                              , "/RESULTS/POST_FATE_evolution_abundance_pixels_"
+                              , "/RESULTS/POST_FATE_evolution_pixels_"
                               , ifelse(length(IDS) <= 5, paste0(IDS, collapse = "_"), length(IDS))
                               , "_"
                               , basename(dir.save)
@@ -192,7 +233,7 @@ POST_FATE.graphic_evolutionPixels = function(
               , row.names = FALSE)
     
     message(paste0("\n The output file \n"
-                   , " > POST_FATE_evolution_abundance_pixels_"
+                   , " > POST_FATE_evolution_pixels_"
                    , ifelse(length(IDS) <= 5, paste0(IDS, collapse = "_"), length(IDS))
                    , "_"
                    , basename(dir.save)
@@ -203,23 +244,37 @@ POST_FATE.graphic_evolutionPixels = function(
     if (opt.doPlot)
     {
       cat("\n PRODUCING PLOT...")
-      vec_col = c('#a6cee3', '#1f78b4', '#b2df8a', '#33a02c'
-                  , '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00'
-                  , '#cab2d6', '#6a3d9a', '#ffff99', '#b15928')
-      fun_col = colorRampPalette(vec_col)
+      vec_col1 = c('#0077BB', '#33BBEE', '#009988', '#EE7733', '#CC3311', '#EE3377')
+      val_col1 = c(rep(rgb(1,1,1,1), no_STRATA)
+                  , colorRampPalette(vec_col1)(no_PFG)
+                  , "grey30")
+      names(val_col1) = c(strata, PFG, "soil")
       
-      ## Evolution of abundance
-      pp = ggplot(distriAbund, aes_string(x = "YEAR", y = "Abund", color = "PFG")) +
-        scale_color_manual("", values = fun_col(no_PFG)) +
-        geom_line(lwd = 0.7) +
-        facet_grid(" ~ ID", scales = ifelse(opt.abund_fixedScale, "fixed", "free_y")) +
+      vec_col2 = c('#FEC44F', '#FB9A29', '#EC7014', '#CC4C02', '#993404', '#662506')
+      val_col2 = colorRampPalette(vec_col2)(no_STRATA)
+      names(val_col2) = strata
+      
+      
+      pp = ggplot(distriAbund, aes_string(x = "YEAR", y = "value")) +
+        geom_line(data = distriAbund[which(distriAbund$TYPE == "soil"),]
+                  , color = "grey30"
+                  , lwd = 0.7) +
+        geom_line(data = distriAbund[which(distriAbund$TYPE == "abundance"),]
+                  , aes_string(color = "GROUP")
+                  , lwd = 0.7) +
+        scale_color_manual("", values = val_col1) +
+        geom_area(data = distriAbund[which(distriAbund$TYPE == "light"),]
+                  , aes_string(fill = "GROUP")
+                  , position = "identity", alpha= 0.4) +
+        scale_fill_manual("", values = val_col2) +
+        facet_grid("TYPE ~ ID", scales = ifelse(opt.abund_fixedScale, "fixed", "free_y")) +
         labs(x = "", y = "", title = paste0("GRAPH A : evolution of species' abundance"),
              subtitle = paste0("For each PFG, the line represents the evolution through time of its abundance\n",
                                "for 5 randomly selected pixels within the studied area.\n")) +
         .getGraphics_theme()
       
       ggsave(filename = paste0(name.simulation
-                               , "/RESULTS/POST_FATE_GRAPHIC_A_evolution_abundance_pixels_"
+                               , "/RESULTS/POST_FATE_GRAPHIC_evolution_pixels_"
                                , ifelse(length(IDS) <= 5, paste0(IDS, collapse = "_"), length(IDS))
                                , "_"
                                , basename(dir.save)
